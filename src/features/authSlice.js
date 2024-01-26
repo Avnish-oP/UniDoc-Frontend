8bf8bcd582f61d25fe335763b9b822e7b0e71b8b
Avnish-oP <kavnish1245@gmail.com>
2024-01-26 13:17:57 +0530

    before adding router and redux setup is done

---
+++ src/features/authSlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const initialState = {
+    user: null,
+    token: null,
+};
+
+const authSlice = createSlice({
+    name : "autht",
+    initialState,
+
+    reducers:{
+        setUser: (state, action)=>{
+            state.user = action.payload;
+    },
+    setToken: (state, action)=>{
+        state.token = action.payload;
+    },
+    logout: (state)=>{
+        state.user = null;
+        state.token = null;
+    }
+    }
+});
+
+
+export const {setUser, setToken, logout} = authSlice.actions;
+
+export default authSlice.reducer;
